97f88f977a3eb95f17bccd6aed7e01e825f42af8
rahanyas <rahanyas3@gmail.com>
2025-01-30 00:24:13 +0530

    creating footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,7 +1,14 @@
+import { GoArrowUpRight } from "react-icons/go";
 const Footer = () => {
   return (
-    <div>
-      <h1>footer</h1>
+    <div className="p-2 bg-gray-800  text-white">
+      <div className="m-4">
+        <h2 className="text-2xl md:text-[30px] capitalize mb-4">ready to experience superior <br /> cooling?</h2>
+        <div className="flex items-center ">
+        <button className='rounded-full px-5 md:px-7 py-2 mr-4 flex items-center border border-white flex-nowrap whitespace-nowrap gap-2 cursor-pointer hover:bg-gray-600 transition ease-in duration-300'>lets talk <GoArrowUpRight /></button>
+        <span className="underline cursor-pointer">info@cooltech.com</span>
+        </div>
+      </div>
     </div>
   )
 };
--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -4,22 +4,20 @@ import { GoArrowUpRight } from "react-icons/go";
 const Home = () => {
   return (
     <div className='p-2'>
-
-      <div className='p-4 mb-2 '>
+      <div className='p-4 md:mb-2'>
         <div className='sansFont'>
          <h1 className='text-[50px] md:text-[64px] capitalize '>your trusted partner in <br /> cooling solution</h1>
          <p className='text-[18px]'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nostrum velit, a rem quo corrupti ad cum nesciunt vel veniam <br /> esse molestias voluptas aliquam tempore culpa. Quasi quae magnam eaque odio?</p>
         </div>
 
-        <div className='m-4 p-2 flex text-capitalize'>
-          <button className='rounded-full px-5 md:px-7 py-2 mr-4 flex items-center bg-blue-500 flex-nowrap whitespace-nowrap'>lets talk <GoArrowUpRight /></button>
-          <button className='rounded-full px-5 md:px-7 py-2 mr-4 flex items-center border flex-nowrap whitespace-nowrap'>explore our products <GoArrowUpRight /></button>
+        <div className='p-2 flex text-capitalize'>
+          <button className='rounded-full px-5 md:px-7 py-2 mr-4 flex items-center bg-blue-500 flex-nowrap whitespace-nowrap gap-2 cursor-pointer'>lets talk <GoArrowUpRight /></button>
+          <button className='rounded-full px-5 md:px-7 py-2 mr-4 flex items-center border flex-nowrap whitespace-nowrap gap-2 cursor-pointer'>explore our products <GoArrowUpRight /></button>
         </div>
 
       </div>
-
       <div className='rounded-md flex justify-center'>
-         <img src={HomeImg} alt="" className='rounded-md  w-[80%] h-[500px] object-contain'/>
+         <img src={HomeImg} alt="" className='rounded-md  w-[80%] h-full object-fill'/>
       </div>
 
     </div>
